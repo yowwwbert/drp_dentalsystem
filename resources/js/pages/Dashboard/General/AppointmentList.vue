ef70b4e88baf838588c94469a54ab59f416ecf6a
yowwwwbert <atencio.robertace@ue.edu.ph>
2025-10-05 18:27:38 +0800

    --

--- resources/js/pages/Dashboard/General/AppointmentList.vue
+++ resources/js/pages/Dashboard/General/AppointmentList.vue
@@ -342,8 +342,6 @@ const getStatusColor = (status: string) => {
           </div>
         </div>
       </div>
-
-      <!-- View Appointment Modal -->
       <div
         v-if="showViewModal"
         class="fixed inset-0 z-50 flex items-center justify-center bg-black/20 backdrop-blur-sm"
